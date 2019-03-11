482ae16e705afc2883991cdaf147464c5e9c822a
adeluleye <adelu4real@gmail.com>
2019-03-11 17:00:46 +0100

    initial server setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// Initailize app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, 
+    () => console.log(`app started on port ${port}`))
